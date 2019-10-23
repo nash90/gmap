3d956d1a914836bd5fb1af372895c527451ffb13
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-10-23 13:32:26 -0700

    improved the script to read city from file and modularized

--- get-data.py
+++ get-data.py
@@ -6,16 +6,28 @@ from datetime import datetime
 from config import settings
 
 key = settings["api_key"]
-q='smoke shop'
+q='restaurant'
 
 gmaps = googlemaps.Client(key=key)
 
 fields = ['place_id','opening_hours','icon', 'formatted_address'] 
 location = '34.110113, -117.715292'
-locationbias= 'circle:2000000@34.110113, -117.715292'
-radius = '20000' 
+#locationbias= 'circle:2000000@34.110113, -117.715292'
+radius = '5000' 
 #place = gmaps.find_place(input="smoke shop", input_type="textquery",
 #fields=fields, location_bias=locationbias)
+data = []
+places = []
+max_count = 2
+
+cities_df = pd.read_csv("la_cities.csv")
+cities_cords = {}
+
+for index, row in cities_df.iterrows():
+  city = row["city"]
+  city_coordinate = str(row["lat"]) + ", " + str(row["lng"])
+  cities_cords[city] = city_coordinate
+
 
 formated_data = {
   "name": "",
@@ -26,51 +38,67 @@ formated_data = {
   "user_rating_total":""
 }
 
-def get_places(q="smoke shop", location='34.110113, -117.715292', radius='40000'):
+def get_places(q="smoke shop", location='34.110113, -117.715292', radius='5000'):
   return gmaps.places(query=q, location=location, radius=radius)
 
-def get_places_with_token(q="smoke shop", location='34.110113, -117.715292', radius='40000', token=''):
+def get_places_with_token(q="smoke shop", location='34.110113, -117.715292', radius='5000', token=''):
   return gmaps.places(query=q, location=location, radius=radius, page_token=token)
 
-res = get_places(q,location,radius)
-places = res["results"]
+def get_places_for_location(q,location,radius, city_name = "none"):
+  global data, places
+  res = get_places(q,location,radius)
+  places = res["results"]
 
-global token 
-token = None
-loop = 0
-max_loop =4
+  token = None
 
-if res["next_page_token"] != None:
-  token = res["next_page_token"]
-
-
-while token != None and loop < max_loop:
-  time.sleep(2)
-  print("executing get_places_with_token loop")
-  res = get_places_with_token(q, location, radius, token)
-  print("break 2")
-  result = res["results"]
-  if result != None and len(result) >1:
-    places = places + res["results"]
-  if "next_page_token" in res:
+  if res["next_page_token"] != None:
     token = res["next_page_token"]
-  else:
-    token = None
-  loop = loop + 1
 
-data = []
-for item in places:
-  obj = {
-  "name": item["name"],
-  "address": item["formatted_address"],
-  "id": item["id"],
-  "rating":item["rating"],
-  "type":", ".join(item["types"]),
-  "user_ratings_total":item["user_ratings_total"],
-  "place_id":item["place_id"]
-  } 
-  data.append(obj)
+  count = 1
+  while token != None and count < max_count:
+    time.sleep(2)
+    print("executing get_places_with_token loop: " + location)
+    res = get_places_with_token(q, location, radius, token)
+    #print("break 2")
+    result = res["results"]
+    if result != None and len(result) >1:
+      places = places + res["results"]
+    if "next_page_token" in res:
+      token = res["next_page_token"]
+    else:
+      token = None
+    count = count+1
 
-df = pd.DataFrame(data)
+  for item in places:
+    obj = {
+    "city": city_name,      
+    "name": item["name"],
+    "address": item["formatted_address"],
+    "id": item["id"],
+    "rating":item["rating"],
+    "type":", ".join(item["types"]),
+    "user_ratings_total":item["user_ratings_total"],
+    "place_id":item["place_id"],
+    "lattitude": item["geometry"]["location"]["lat"],
+    "longitude": item["geometry"]["location"]["lng"]
+    } 
+    data.append(obj)
 
-df.to_csv("data.csv")
+
+
+def save_csv():
+  df = pd.DataFrame(data)
+
+  df.to_csv("data.csv")
+
+def run_script():
+  #get_places_for_location(q,location,radius)
+
+  for key, value in cities_cords.items():
+    get_places_for_location(q, value, radius, key)
+    #if key == "Avocado Heights":
+      #break
+
+  save_csv()
+
+run_script()
